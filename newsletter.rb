feda471c9de608a58eda6d0192f26d1d8ff41e23
ianmcknnn <ianmcnnn@gmail.com>
2020-06-02 11:47:56 -0400

    working newsletter code

--- newsletter.rb
+++ newsletter.rb
@@ -28,33 +28,44 @@ ARTICLES = [
 #########################
 
 def calculate_recipients
+  SUBSCRIBERS.difference(UNSUBSCRIBED)
   # Using the SUBSCRIBERS and UNSUBSCRIBED arrays,
   # write a method that will return an array of only the subscribers who haven't unsubscribed
 end
 
-def first_n_articles(number_of_articles
+
+def first_n_articles(number_of_articles)
   ARTICLES.first(number_of_articles)
 end
 
 def print_recipients
+  recipients = calculate_recipients
+  print recipients.join(', ')
+  puts "\n"
   # Write a method that uses the output of calculate_recipients
   # and returns a list of emails separated by commas
   # Ex) "abc@email.com, def@email.com, ghi@email.com"
 end
 
 def print_one_article(article)
+  puts article[:title]
+  puts "by: #{article[:author]}"
+  puts article[:text]
+  puts "\n"
   # Write a method that will take an article hash
   # and print the title, author and text as a formatted string
   # See the README/sample output for examples
 end
 
 def print_many_articles(articles)
+  articles.each {|article| print_one_article(article)}
+  
   # Write a method that will take in an array of article hashes
   # and format each one using the print_one_article method
 end
 
 def format_campus_location(campus)
-  "Flatiron #{campus["name"]}"
+  "Flatiron #{campus[:name]}"
 end
 
 def format_subject
@@ -80,13 +91,12 @@ def print_newsletter(number)
   print_many_articles(articles)
   puts format_footer(CAMPUS)
 
-  end
 end
 
 def run
   # We want our program to print three articles by default,
   # but we can change that number here
-  print_newsletter("3")
+  print_newsletter(5)
 end
 
 # When we run "ruby newsletter.rb" in the command line,
